feb9664746adda0f69242b85e30a7b1b92957d59
bieltv3 <bieltv.3@gmail.com>
2013-03-06 12:55:18 +0100

    update sources

--- device_cori.mk
+++ device_cori.mk
@@ -12,22 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Call common.mk
+# Inherit products
 $(call inherit-product, device/samsung/bcm21553-common/common.mk)
-
-# Inherit from those products. Most specific first.
+$(call inherit-product, vendor/samsung/cori/vendor_blobs.mk)
 $(call inherit-product, build/target/product/languages_full.mk)
 $(call inherit-product, build/target/product/full_base.mk)
 
 # Add device package overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/cori/overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/cori/ldpi
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/mdpi
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-# Add LDPI assets, in addition to LDPI
+# Add LDPI assets
     PRODUCT_LOCALES += ldpi
+
+# Torch
+PRODUCT_PACKAGES += \
+    Torch
 
 # Kernel modules
 PRODUCT_COPY_FILES += \
@@ -43,13 +46,6 @@ PRODUCT_COPY_FILES += \
     device/samsung/cori/ramdisk/ueventd.gt-s5300.rc:root/ueventd.gt-s5300.rc \
     device/samsung/cori/ramdisk/init.gt-s5300.rc:root/init.gt-s5300.rc
 
-# Use the non-open-source parts, if they're present
--include vendor/samsung/cori/BoardConfigVendor.mk
-
-# Bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
-
 # Other kernel modules not in ramdisk
 ifeq ($(TARGET_PREBUILT_KERNEL),)
     LOCAL_KERNEL := device/samsung/cori/prebuilt/kernel
@@ -59,9 +55,3 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
-
-# See comment at the top of this file. This is where the other
-# half of the device-specific product definition file takes care
-# of the aspects that require proprietary drivers that aren't
-# commonly available
-$(call inherit-product-if-exists, vendor/samsung/cori/cori-vendor.mk)
